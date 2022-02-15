f9ec4d29230ce6012ddbc53ecd7e8ddfbf7816d7
smmne <kdaumh33@gmail.com>
2022-02-15 12:18:46 +0900

    css module, sass module

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import './App.css';
+// import "./App.scss";
+import styles from "./App.module.css";
+
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles["App"]}>
+      <header className={styles["header"]}>
+        <img src={logo} className={styles["logo"]} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Button</Button>
       </header>
     </div>
   );
